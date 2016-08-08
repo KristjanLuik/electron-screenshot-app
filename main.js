e92deca330a31bb5fb9f3d557080c8961f7fec08
Kristjan Luik <kristjanluik1@gmail.com>
2016-08-08 23:18:59 +0300

    will add a timestamp as default filename, and doesnt crash on cancel on save.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = electron;
 const ipcMain = require('electron').ipcMain;
 const {dialog} = require('electron');
 var fs = require("fs");
-var asi;
+var mainWindow;
 
 
 app.on('ready', () => {
@@ -11,23 +11,25 @@ app.on('ready', () => {
     win.loadURL(`file://${__dirname}/index.html`);
 
     //asi = win.webContents;
-    asi = win;
+    mainWindow = win;
     //win.openDevTools();
 
 });
 
 ipcMain.on('shot', function (event, arg) {
     arg = arg.replace(/^data:image\/\w+;base64,/, "");
-    var path = dialog.showSaveDialog({title:'Where to save you S#ot' ,defaultPath: 'asi.png'});
-    fs.writeFile(path, new Buffer(arg,"base64"), function (er) {
-        console.log(er);
-    });
-    asi.show();
+    var path = dialog.showSaveDialog({title:'Where to save you S#ot' ,defaultPath: `${Date.now()}-screenshot.png`});
+    if (path) {
+        fs.writeFile(path, new Buffer(arg,"base64"), function (er) {
+            console.log(er);
+        });
+    }
+
+    mainWindow.show();
 });
 
 exports.takeScreenShot = () => {
-    //console.log(asi);
-    asi.hide();
+    mainWindow.hide();
     let win = new BrowserWindow({width: 200, height: 200, fullscreen: true, skipTaskbar: true, frame: false, transparent: true});
     win.loadURL(`file://${__dirname}/shot.html`);
 };
